2c9b4cb5d65c78785df8636def6669798ca48e68
Juan De Jesus <juandejesus@Juans-MacBook-Pro.local>
2023-06-07 20:49:13 -0500

    Creates font sizes for various content - UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from "react";
-import "./App.scss"
+import "./App.scss";
 
 const App = () => {
-    return (
-        <>
-            Hello World
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className="title">Podcaster</h1>
+      <h2 className="episode__number">Episode: 33</h2>
+      <h2 className="card__title">
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+        archivos de texto.{" "}
+      </h2>
+      <h2 className="card__title">
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+        archivos de texto.{" "}
+      </h2>
+      <p className="card__author">
+        Author: Lorem Ipsum es simplemente el texto
+      </p>
+      <h3 className="summary-podcast__title">Innocence</h3>
+      <p className="summary-podcast__author">By Kenny G</p>
+      <p className="summary-podcast__description">
+        Es un hecho establecido hace demasiado tiempo que un lector se distraerá
+        con el contenido del texto de un sitio mientras que mira su diseño. El
+        punto de usar Lorem Ipsum es que tiene una distribución más o menos
+        normal de las letras
+      </p>
+    </>
+  );
+};
 
 export default App;
