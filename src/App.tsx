c48195846d3598befb2a31b17c90a335cd4ef7ab
Juan De Jesus <juandejesus@Juans-MacBook-Pro.local>
2023-06-08 21:05:58 -0500

    Creates main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import "./App.scss";
-import CardList from "./components/card-list/card-list";
-import Card from "./components/card/card";
-import CounterBar from "./components/counter-bar/counter-bar";
-import DataGrid from "./components/data-grid/data-grid";
-import EpisodeDetail from "./components/episode-detail/episode-detail";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
-import Message from "./components/message/message";
-import Spinner from "./components/spinner/spinner";
-import Summary from "./components/summary/summary";
-import TextInput from "./components/text-input/text-input";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className="main">Hello World</main>
       <Footer />
     </>
   );
--- src/models/models.tsx
+++ src/models/models.tsx
@@ -1,18 +1,18 @@
 export interface Multimedia {
-    id:string,
-    title: string;
-    description?: string;
-    author?: string;
+  id: string;
+  title: string;
+  description?: string;
+  author?: string;
 }
 
 export interface Podcast extends Multimedia {
-    thumbnailAlt?: string,
-    thumbnailURL?: string
+  thumbnailAlt?: string;
+  thumbnailURL?: string;
 }
 
 export interface Episode extends Multimedia {
-    date?: string;
-    duration?: string;
-    audioURL?: string,
-    episodeFileExtension?: string
+  date?: string;
+  duration?: string;
+  audioURL?: string;
+  episodeFileExtension?: string;
 }
