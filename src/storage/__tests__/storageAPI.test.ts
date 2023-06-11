dd14239d9308237d194348f8e57291625fea748c
Juan De Jesus <juandejesus@Juans-MacBook-Pro.local>
2023-06-11 15:29:16 -0500

    Adds unit test for storageAPI

--- src/storage/__tests__/storageAPI.test.ts
+++ src/storage/__tests__/storageAPI.test.ts
@@ -1,29 +1,57 @@
 import { listOfPodcasts } from "../../helpers/data";
-import { PODCASTS_STORAGE_KEY, saveData } from "../storageAPI";
+import { PODCASTS_STORAGE_KEY, retrieveData, saveData } from "../storageAPI";
 
-import localStorageMock from "../../../mocks/localStorageMock"
+import localStorageMock from "../../../mocks/localStorageMock";
+import { toStorageFormat } from "../../helpers/helpers";
+import { Episode, Podcast } from "../../models/models";
 
-describe("Storage API" , () => {
-    const setItemMocked = jest.fn()
+describe("Storage API", () => {
+  const setItemMocked = jest.fn();
+  let fetchDataMocked: jest.Mock<Promise<Podcast[] | Episode[]>>;
+  let getItemMocked: jest.Mock<string>;
+  let getItemNoDataMocked: jest.Mock<string>;
+  let afterCallbackMocked: jest.Mock<Podcast[] | Episode[]>;
 
-    beforeAll(() => {
-        Object.defineProperty(window, "localStorage", { value: localStorageMock });
-        window.localStorage.setItem = setItemMocked;
-    })
+  beforeEach(() => {
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    window.localStorage.setItem = setItemMocked;
+    fetchDataMocked = jest.fn(() => Promise.resolve(listOfPodcasts));
+    getItemMocked = jest.fn(() => toStorageFormat(listOfPodcasts));
+    afterCallbackMocked = jest.fn(() => listOfPodcasts);
+    getItemNoDataMocked = jest.fn(() => undefined);
+  });
 
-    afterAll(() => {
-        window.localStorage.clear();
-    })
+  afterEach(() => {
+    window.localStorage.clear();
+  });
 
-    describe("Storage API" , () => {
-        const fetchDataMocked = jest.fn(() => Promise.resolve(listOfPodcasts));
-    
-        it("should save a list of podcasts", async () => {
-            const storageKey = PODCASTS_STORAGE_KEY;
-            const storagedData = await saveData(storageKey, fetchDataMocked);
-            expect(fetchDataMocked).toHaveBeenCalledTimes(1) 
-            expect(storagedData).toEqual(listOfPodcasts)  
-            expect(setItemMocked).toHaveBeenCalled()
-        })
-    })  
-}) 
+  describe("Storage API", () => {
+    it("should save a list of podcasts", async () => {
+      const storageKey = PODCASTS_STORAGE_KEY;
+      const storagedData = await saveData(storageKey, fetchDataMocked);
+      expect(fetchDataMocked).toHaveBeenCalledTimes(1);
+      expect(storagedData).toEqual(listOfPodcasts);
+      expect(setItemMocked).toHaveBeenCalled();
+    });
+
+    it("should retrieve a list of podcasts from the local storage when less than 24 houras has passed by", async () => {
+      const storageKey = PODCASTS_STORAGE_KEY;
+      window.localStorage.getItem = getItemMocked;
+      const storagedData = await retrieveData(storageKey, fetchDataMocked, afterCallbackMocked);
+      expect(fetchDataMocked).not.toHaveBeenCalled;
+      expect(storagedData).toEqual(listOfPodcasts);
+      expect(getItemMocked).toHaveBeenCalled();
+      expect(afterCallbackMocked).toHaveBeenCalled();
+    });
+
+    it("should retrieve a list of podcasts from the service when less than 24 houras has passed by", async () => {
+      const storageKey = PODCASTS_STORAGE_KEY;
+      window.localStorage.getItem = getItemNoDataMocked;
+      const storagedData = await retrieveData(storageKey, fetchDataMocked, afterCallbackMocked);
+      expect(fetchDataMocked).toHaveBeenCalled;
+      expect(storagedData).toEqual(listOfPodcasts);
+      expect(getItemNoDataMocked).toHaveBeenCalled();
+      expect(afterCallbackMocked).not.toHaveBeenCalled();
+    });
+  });
+});
